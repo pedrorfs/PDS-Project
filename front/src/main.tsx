9f57280b989d9956c8ed948c2cf3cdb255aaffb9
pedrorfs <pedrorfs11@gmail.com>
2024-03-24 12:00:12 -0300

    Add Topbar component

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App.tsx';
+import Home from './components/Home/Home.tsx';
 import Login from  './pages/Login/Login.tsx';
+import DefaultPage from './pages/Default/DefaultPage.tsx';
 import './style.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Login />
+    <DefaultPage />
   </React.StrictMode>,
 )
