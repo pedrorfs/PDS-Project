4d5bcbc0aa58514cf1aeeec0e895af889c0c0d44
João Vitor Bicalho <joaovitorbicalho.dasilva@gmail.com>
2024-03-24 18:42:52 -0300

    add routes config

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 import App from './App.tsx';
 import Home from './components/Home/Home.tsx';
-import Login from  './pages/Login/Login.tsx';
+import Login from './pages/Login/Login.tsx';
 import DefaultPage from './pages/Default/DefaultPage.tsx';
+
 import './style.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <DefaultPage />
+    <BrowserRouter>
+      <Routes>
+      <Route path='/login' element={<Login />} />
+        <Route element={<App />}>
+          <Route path='/' element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
