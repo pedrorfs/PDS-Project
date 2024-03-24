c81e949160236ea51653211f2c1cfb9dee491c01
pedrorfs <pedrorfs11@gmail.com>
2024-03-24 12:30:59 -0300

    Add DefaultPage component

---
+++ front/src/pages/Default/DefaultPage.tsx
@@ -0,0 +1,18 @@
+import "./default-page.scss";
+import Sidebar from "./../components/Sidebar/Sidebar";
+import Topbar from "./../components/Topbar/Topbar";
+import Home from "./../components/Home/Home";
+
+export default function DefaultPage() {
+
+
+    return (
+        <div className="default-page">
+            <Topbar />
+            <div className="default-page__bottom">
+                <Sidebar />
+                <Home />
+            </div>
+        </div>
+    )
+}
