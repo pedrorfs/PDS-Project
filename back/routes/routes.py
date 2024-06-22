51626961a78527289205544e8ef90fef0321a077
NidaBoy <daniboytorres99xx@gmail.com>
2024-06-21 22:51:11 -0300

    Modificando a criação de usuários

--- back/routes/routes.py
+++ back/routes/routes.py
@@ -1,15 +1,18 @@
 from flask import request
 from app import app, repository
+from services.user_services import save_new_user
 
-import domain.user as user, adapters.SQLITE3_repository as SQLITE3_repository
+from domain.user import User
 
 @app.route('/api/user/new', methods=['POST'])
 def create_user_route():
     # TODO: Validate json request data
-    user.User(**request.json).persist(repository)
+    user = User(**request.json)
+    
+    save_new_user(user, repository)
     return 'Success', 201
 
 
 @app.route('/')
 def index():
-    return "Hello World!!!!!"
+    return "Hello World!"
--- back/services/user_services.py
+++ back/services/user_services.py
@@ -1,1 +1,5 @@
 from domain.user import User
+#from app import repository
+
+def save_new_user(user, repository):
+    repository.insert_user(user)
