e4c9c9330352d6948d966c3e491a9a962c32959c
João Vítor Bicalho <joaovitorbicalho.dasilva@gmail.com>
2024-07-03 19:07:33 -0300

    fix homepage

--- front/src/components/Home/Home.tsx
+++ front/src/components/Home/Home.tsx
@@ -1,24 +1,71 @@
+import { useEffect, useState } from "react";
+
 import "./home.scss";
+
+import { getUserData } from "../../requests/User/GetUserData";
+import { getBuyStocks } from "../../requests/Invest/GetBuyStock";
+
+
+interface BuyStock {
+    Code: string
+    Name: string
+    Price: number
+    Quantity: number
+}
 
 export function Home() {
 
+    const [myStocks, setMyStocks] = useState<BuyStock[]>([])
+
+    const [balance, setBalance] = useState(0)
+
+    const getUser = async () => {
+
+        const response = await getUserData()
+
+        setBalance(response.data.balance)
+
+    }
+
+    const getUserStocks = async () => {
+
+        const response = await getBuyStocks()
+
+        console.log(response)
+
+        setMyStocks(response)
+        console.log('minhas ações')
+        console.log(myStocks)
+
+    }
+
+    useEffect(() => {
+        getUser()
+        getUserStocks()
+    }, [])
+
+    let totalInvestValue = 0;
+
+    for (const stock in myStocks) {
+        totalInvestValue += (myStocks[stock].Price * myStocks[stock].Quantity)
+    }
 
     return (
         <div className="home">
-            <div className="home__block">
+            <div className="home__block first-line">
                 <h3>Meus investimentos</h3>
-                <p>R$1.000.000,00</p>
+                <p>{totalInvestValue.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
             </div>
-            <div className="home__block graficos">
+            {/* <div className="home__block graficos">
                 <h3>Graficos</h3>
-            </div>
+            </div> */}
             <div className="home__block valor-aplicado">
-                <h3>Valor aplicado</h3>
-                <p>R$10.000,00</p>
+                <h3>Saldo</h3>
+                <p>{balance.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
             </div>
             <div className="home__block rendimento-total">
                 <h3>Rendimento Total</h3>
-                <p>R$500,00</p>
+                <p>R$0,00</p>
             </div>
             <div className="home__block message">
                 <p>Investir com frequência é o segredo para chegar cada vez mais longe</p>
