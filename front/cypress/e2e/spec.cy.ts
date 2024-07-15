98f5e5a08bc96b0558aec26f597ed068b9d8da56
João Vítor Bicalho <joaovitorbicalho.dasilva@gmail.com>
2024-07-15 15:01:11 -0300

    add logout test

--- front/cypress/e2e/spec.cy.ts
+++ front/cypress/e2e/spec.cy.ts
@@ -34,4 +34,13 @@ describe('Teste Cadastro', () => {
     cy.get('#name_input').type('{selectall}{backspace}').type('Joao Vitor Bicalho')
     cy.get('.personal-data__button').click()
   })
+
+  it('logout', () => {
+    cy.get('.initial__content__access-account').click()
+    cy.get('#cpf_input_login').type('33333333333')
+    cy.get('#password_input_login').type('123456')
+    cy.get('.login__button button').click()
+    cy.get('.topbar__dropdown').click()
+    cy.get('.topbar__dropdown__options #topdar_dropdown_option_logout').click()
+  })
 })
